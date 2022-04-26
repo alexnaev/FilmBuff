5fe1a3d55c7d9c392db9466866e537c9e469bd49
alexnaev <102225427+alexnaev@users.noreply.github.com>
2022-04-26 11:29:29 +0200

    Trying to fix some error

--- FilmBuff/Controllers/MoviesController.cs
+++ FilmBuff/Controllers/MoviesController.cs
@@ -1,8 +1,10 @@
 ﻿using FilmBuff.Data;
+using FilmBuff.Models.Database;
 using FilmBuff.Models.Settings;
 using FilmBuff.Services.Interfaces;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
 
 namespace FilmBuff.Controllers
 {
@@ -14,9 +16,9 @@ namespace FilmBuff.Controllers
         private readonly IRemoteMovieService _tmdbMovieService;
         private readonly IDataMappingService _tmdbMappingService;
 
-        public MoviesController(AppSettings appSettings, ApplicationDbContext context, IImageService imageService, IRemoteMovieService tmdbMovieService, IDataMappingService tmdbMappingService)
+        public MoviesController(IOptions<AppSettings> appSettings, ApplicationDbContext context, IImageService imageService, IRemoteMovieService tmdbMovieService, IDataMappingService tmdbMappingService)
         {
-            _appSettings = appSettings;
+            _appSettings = appSettings.Value;
             _context = context;
             _imageService = imageService;
             _tmdbMovieService = tmdbMovieService;
@@ -28,6 +30,67 @@ namespace FilmBuff.Controllers
             var movies = await _context.Movie.ToListAsync();
 
             return View(movies);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Import(int id)
+        {
+            //Prevent duplacte imports
+            if(_context.Movie.Any(m => m.MovieId == id))
+            {
+                var localMovie = await _context.Movie.FirstOrDefaultAsync(m => m.MovieId == id);
+                return RedirectToAction("Details", "Movies", new {id = localMovie.Id, local = true});
+            }
+
+            //Get raw data from API
+            var movieDetail = await _tmdbMovieService.MovieDetailAsync(id);
+
+            //Run daata trough mapping procedure
+            var movie = _tmdbMappingService.MapMovieDetailAsync(movieDetail);
+
+            //Add new movie
+            _context.Add(movie);
+            await _context.SaveChangesAsync();
+
+            //Assign to default "All" collection
+            await AddToMovieCollection(movie.Id, _appSettings.FilmBuffSettings.DefaultCollection.Name);
+
+            return RedirectToAction("Import");
+        }
+
+        public async Task<IActionResult> Library()
+        {
+            var movies = await _context.Movie.ToListAsync();
+
+            return View(movies);
+        }
+
+        private async Task AddToMovieCollection(int movieId, string collectionName)
+        {
+            var collection = await _context.Collection.FirstOrDefaultAsync(c => c.Name == collectionName);
+            _context.Add(
+                new MovieCollection()
+                {
+                    CollectionId = collection.Id,
+                    MovieId = movieId,
+                }
+            );
+
+            await _context.SaveChangesAsync();
+        }
+
+        private async Task AddToMovieCollection(int movieId, int collectionId)
+        {
+            _context.Add(
+                new MovieCollection()
+                {
+                    CollectionId = collectionId,
+                    MovieId = movieId,
+                }
+            );
+
+            await _context.SaveChangesAsync();
         }
     }
 }
